66c6afa1d557540c8c402d723dc26f0fe6c7e90d
Ricky Wong <rickyltwong2+gh@gmail.com>
2023-05-14 11:38:55 -0400

    initial commit

---
+++ mydb.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+# connect server
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'rickyltwong',
+    passwd = 'password'
+
+)
+
+# prepare a cursor object
+cursorObject = dataBase.cursor()
+
+# create a database
+cursorObject.execute("CREATE DATABASE dundermifflin")
+
+print("All Done!")
